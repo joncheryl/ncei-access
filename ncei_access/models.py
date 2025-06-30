ffecf1a7db2e6696e0a517fa5ead5101e33e29f6
joncheryl <prof.sherrill@gmail.com>
2025-06-30 14:03:03 -0600

    added function to determine elevation of weather station

--- ncei_access/models.py
+++ ncei_access/models.py
@@ -39,7 +39,7 @@ class Station:
         :param lat: Latitude of station. Decimal format.
         :param lon: Longitude of station. Decimal format.
         :param dataTypes: Dict of dataTypes reported with a tuple for each (startDate, endDate)
-        """ # pylint: disable=line-too-long
+        """  # pylint: disable=line-too-long
         self.name = name
         self.station_id = station_id
         self.lat = lat
@@ -57,7 +57,7 @@ class Station:
         :param start_date: data_type records should begin on or before start_date, defaults to None
         :param end_date: data_type records should end on or after end_date, defaults to None
         :return: boolean
-        """ # pylint: disable=line-too-long
+        """  # pylint: disable=line-too-long
         type_dict = next((d for d in self.data_types if d["id"] == data_type), None)
 
         if not type_dict:
@@ -97,3 +97,41 @@ class Station:
         c = 2 * atan2(sqrt(a), sqrt(1 - a))
         distance = r * c
         return distance
+
+    def elevation(
+        self,
+        start_date: str = None,
+        end_date: str = None,
+    ) -> float:
+        """Return the elevation of the station in meters. If start and/or end dates are
+        provided, return the elevations if it's recorded during that period. If no dates are
+        provided, return the most recent elevation record. This is actually kindof a hack
+        because the NCEI API doesn't seem to include station elevation in the station
+        metadata but it *is* recorded as a daily data type. So we just get the daily
+        elevation data and return the most-ish recent record or all records if dates are
+        provided.
+
+        :param start_date: Optional start date to check for elevation records, defaults to '2015-01-01'
+        :param end_date: Optional end date to check for elevation records, defaults to '2025-01-01'
+        :return: Elevation in meters.
+        """  # pylint: disable=line-too-long
+
+        # Importing here to avoid circular import issues
+        from ncei_access import ncei_accessor as na
+
+        ncei_db = na.NceiAccessor()
+
+        elevation_data = ncei_db.get_daily(
+            data_types="ELEVATION",
+            stations=self.station_id,
+            start=start_date if start_date else "2015-01-01",
+            end=end_date if end_date else "2025-01-01",
+        )
+
+        if start_date or end_date:
+            return [
+                {"DATE": x.get("DATE"), "ELEVATION": float(x.get("ELEVATION"))}
+                for x in elevation_data
+            ]
+
+        return float(elevation_data[-1].get("ELEVATION"))
